fe5294a73c17ed576799671dd61a9eabdf3a4147
Likai Liu <liulk@cs.bu.edu>
2010-06-07 23:34:22 +0000

    (WIP) various makefile related work.
      - Deleted old makefiles that are no longer needed (being reworked)
      - Removed test.sh.in (will need to add back its equivalent later)
      - Make.bootstrap0: tested, works.
      - Make.bootstrap1: mostly works, link error.

--- ATSHOME_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ${ATSHOME} != ${PWD} ] ; then
-  echo "The value of ATSHOME is set to \"${ATSHOME}\", but it should be set to \"${PWD}\"!"; exit 1;
-fi
-
-### end of [ATSHOME_check.sh] ###
