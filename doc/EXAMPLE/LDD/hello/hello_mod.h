538d5a130092c6bd274a89f0c1f2136447d65bbb
Hongwei Xi <hwxi@cs.bu.edu>
2011-02-23 05:21:29 +0000

    Updating.

--- doc/EXAMPLE/LDD/hello/hello_mod.h
+++ doc/EXAMPLE/LDD/hello/hello_mod.h
@@ -1,15 +1,25 @@
+#ifndef HELLO_MOD_H
+#define HELLO_MOD_H
 //
 // from ${ATSHOME}/ccomp/runtime/ats_types.h
 //
 typedef int ats_int_type ;
 typedef void ats_void_type ;
 typedef void *ats_ptr_type ;
-
 //
-// copy from ${ATSHOME}/ccomp/runtime/ats_basic.h
+// from ${ATSHOME}/ccomp/runtime/ats_basic.h
 //
 #define ATSunused __attribute__ ((unused))
 #define ATSlocal(ty, var) ty ATSunused var
-#define ATSlocal_void(var)
+#define ATSlocal_void(var) // void var
+//
+// for handling a call like: printk (KERN_INFO "...")
+//
+#ifdef ATSstrcst
+#undef ATSstrcst
+#endif
+#define ATSstrcst(x) x
+//
+#endif
 
 /* end of [hello_mod.h] */
--- doc/EXAMPLE/LDD/hello/hello_mod.sats
+++ doc/EXAMPLE/LDD/hello/hello_mod.sats
@@ -12,13 +12,6 @@
 //
 #include "hello_mod.h"
 //
-// for handling a call like: printk (KERN_INFO "...")
-//
-#ifdef ATSstrcst
-#undef ATSstrcst
-#endif
-#define ATSstrcst(x) x
-//
 %} // end of [%{#]
 
 (* ****** ****** *)
